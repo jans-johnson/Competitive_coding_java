a40629bfd486f903f8f8b05163620bf41582f2de
Jans Johnson <jaansjohnson@gmail.com>
2023-04-09 04:07:32 +0530

    Longest Common subarray Dynamic Programming edit

--- Dynamic Programming/LongestCommonSub.java
+++ Dynamic Programming/LongestCommonSub.java
@@ -5,14 +5,15 @@ public class LongestCommonSub {
         for(int i=1;i<=i1;i++)
         for(int j=1;j<=i2;j++)
         {
-            arr[i][j]=Integer.max(arr[i-1][j], arr[i][j-1]);
             if(s1.charAt(i-1)==s2.charAt(j-1))
-                arr[i][j]+=1;
+                arr[i][j]=arr[i-1][j-1]+1;
+            else
+            arr[i][j]=Integer.max(arr[i-1][j], arr[i][j-1]);
         }
         return arr[i1][i2];
     }
     public static void main(String[] args) {
-        String s1="ABCDE",s2="A!B#C$D";
-        System.out.println(longest(s1,5,s2,7));
+        String s1="ABCDE",s2="ABCDE";
+        System.out.println(longest(s1,5,s2,5));
     }
 }
